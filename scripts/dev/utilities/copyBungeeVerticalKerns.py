af26b8b8a93d19187bb5c9103d5145a75fe01565
David Jonathan Ross <david@djr.com>
2016-01-04 08:36:25 -0500

    remove undocumented utilities

--- scripts/dev/utilities/copyBungeeVerticalKerns.py
+++
@@ -1,9 +0,0 @@
-f = CurrentFont()
-src = AllFonts()[1]
-f.kerning.clear()
-f.groups.clear()
-
-for groupName, groupGlyphs in src.groups.items():
-    f.groups[groupName] = groupGlyphs
-
-f.kerning.update(src.kerning.asDict())
--- scripts/dev/utilities/replaceAlt001WithComponents.py
+++
@@ -1,8 +0,0 @@
-f = CurrentFont()
-for g in f:
-    if '.alt001' in g.name:
-        print g.name
-        sourceName = g.name.replace('.alt001', '')
-        g.clearContours()
-        g.clearComponents()
-        g.appendComponent(sourceName)
