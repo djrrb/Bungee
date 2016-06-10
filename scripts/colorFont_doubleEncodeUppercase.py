97f7612b01cf3150df49d021f7e75e6e73f2ed2a
David Ross <david@djr.com>
2016-06-10 12:04:18 -0400

    fix reference to upperToSingleLower

--- scripts/colorFont_doubleEncodeUppercase.py
+++ scripts/colorFont_doubleEncodeUppercase.py
@@ -1,10 +1,9 @@
-from fbits.constants import Constants as C
-o = C.UNICODE_UPPER_TO_SINGLE_LOWER
+from compositor.caseConversionMaps import upperToSingleLower
 f = CurrentFont()
 for g in f:
     gname = g.name
     for u in g.unicodes:
-        if o.has_key(u):
-            l = o[u]
+        if upperToSingleLower.has_key(u):
+            l = upperToSingleLower[u]
             g.unicodes = g.unicodes + [l]
             
