c33f5de6f5c1fa952372ebb20ab0ee9d40178bf1
Marte Verhaegen <marte.verhaegen@gmail.com>
2023-11-16 14:39:16 +0100

    Add shell script for fontbakery checks

---
+++ run_fontbakery.sh
@@ -0,0 +1,14 @@
+mkdir build/fontbakery/
+
+for folder in Bungee_Basic Bungee_Layers Bungee_Rotated Bungee_Color
+do
+    for ttf in build/fonts/$folder/*.ttf
+    do
+		fontbakery check-googlefonts \
+			-x com.google.fonts/check/alt_caron \
+			-x com.google.fonts/check/contour_count \
+			-x com.google.fonts/check/vertical_metrics_regressions \
+			--html build/fontbakery/$(basename $ttf .ttf).html \
+			$ttf
+    done
+done
