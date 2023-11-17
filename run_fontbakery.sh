03d2ec370f18cf3a04d285fb06f1d5102c65674a
Marte Verhaegen <marte.verhaegen@gmail.com>
2023-11-17 17:29:03 +0100

    Re-indent with spaces

--- run_fontbakery.sh
+++ run_fontbakery.sh
@@ -9,12 +9,12 @@ for folder in Bungee_Basic Bungee_Layers Bungee_Rotated Bungee_Color
 do
     for ttf in build/fonts/$folder/*.ttf
     do
-		fontbakery check-googlefonts \
-			-x com.google.fonts/check/alt_caron \
-			-x com.google.fonts/check/contour_count \
-			-x com.google.fonts/check/glyphsets/shape_languages \
-			-x com.google.fonts/check/vertical_metrics_regressions \
-			--html build/fontbakery/$(basename $ttf .ttf).html \
-			$ttf
+        fontbakery check-googlefonts \
+            -x com.google.fonts/check/alt_caron \
+            -x com.google.fonts/check/contour_count \
+            -x com.google.fonts/check/glyphsets/shape_languages \
+            -x com.google.fonts/check/vertical_metrics_regressions \
+            --html build/fontbakery/$(basename $ttf .ttf).html \
+            $ttf
     done
 done
